c965f409e07a7046e1bc85cc822799832bcc615b
Inderjot Singh <inderjotsingh27@gmail.com>
2021-09-09 17:08:47 +0530

    End of Section 5

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,51 @@
+import { User } from './../_models/user';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { ReplaySubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new ReplaySubject<User>(1);  // Size of buffer (1)
+  // ReplaySubject: Is kind of buffer object. It stores the value inside here and anytime subscriber subscribes, it
+  // is going to emit the last value or how many values we need to subscribe.
+
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: any): void => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+      map((user: any) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(undefined);
+  }
+}
